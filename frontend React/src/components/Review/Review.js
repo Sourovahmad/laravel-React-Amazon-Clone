6e6402e25193de4b0710503f3492cedd0ab7ccfc
sourovahmad <mdsourovahmad19@gmail.com>
2021-11-04 09:05:11 +0600

    started Review page

--- frontend React/src/components/Review/Review.js
+++ frontend React/src/components/Review/Review.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getStoredCart } from '../../utilities/fakedb';
 
 const Review = () => {
+
+    const [orderdItems, setorderdItems] = useState([]);
+
+    useEffect(()=>{
+       const myCarts = getStoredCart();
+       const items = Object.keys(myCarts);
+
+
+    const prod   items.map(key => {
+
+       })
+  
+     
+    },[])
+
+
     return (
         <div>
-            <h1> hey man iam review page </h1>
+           {orderdItems}
         </div>
     );
 };
